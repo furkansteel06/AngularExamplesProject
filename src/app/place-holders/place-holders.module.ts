52f7546f6355e6cc3fb1b7ed0b27eef237e346f0
Muhammet Furkan CELIK <muhammetfurkancelik@outlook.com>
2022-05-06 13:52:50 +0300

    added shared module and placeholder comp

--- src/app/place-holders/place-holders.module.ts
+++ src/app/place-holders/place-holders.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PlaceHoldersRoutingModule } from './place-holders-routing.module';
 import { PlaceholdersHomeComponent } from './placeholders-home/placeholders-home.component';
+import { PlaceholdersComponent } from './placeholders/placeholders.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [PlaceholdersHomeComponent],
+  declarations: [PlaceholdersHomeComponent, PlaceholdersComponent],
   imports: [
     CommonModule,
-    PlaceHoldersRoutingModule
+    PlaceHoldersRoutingModule,
+    SharedModule
   ],
   exports:[
     // PlaceholdersHomeComponent
