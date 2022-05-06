65342d3dea3e2a47c3fce079651da7e180a3b221
Muhammet Furkan CELIK <muhammetfurkancelik@outlook.com>
2022-05-06 10:18:12 +0300

    Modules are created # ng g m moduleName --routing

---
+++ src/app/popups/popups.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PopupsRoutingModule } from './popups-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PopupsRoutingModule
+  ]
+})
+export class PopupsModule { }
